aef379a4f8427fe8957f432a7465fcd8896f85a3
cgamemfgc <cgamemfgc@gmail.com>
2024-11-09 22:57:19 +0900

    完成

---
+++ ScreenTransition/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  ScreenTransition
+//
+//  Created by Takeshi Sakamoto on 2024/11/09.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
---
+++ ScreenTransition/ThirdView.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdView.swift
+//  ScreenTransition
+//
+//  Created by Takeshi Sakamoto on 2024/11/09.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThirdView()
+}
